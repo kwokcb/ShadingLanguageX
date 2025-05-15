b94d0361f63f2a854b7573014bd3e7ed0bba248b
JAKE-PC <jakethorn1.1@gmail.com>
2025-05-15 06:27:03 +0200

    uploaded houdini shelf tool

---
+++ tools/houdini_shelf_tool.py
@@ -0,0 +1,36 @@
+import subprocess
+import time
+from threading import Thread
+
+# Re-compiles and updates houdini references to MaterialX files.
+
+# path to mxslc executable
+MXSLC_PATH = r"your\path\to\mxslc.exe"
+
+# path to .mxsl file or directory containing .mxsl files
+SHADER_PATH = r"your\path\to\shaders"
+
+
+def compile_and_enable_references():
+    # execute mxslc and print output
+    result = subprocess.run([MXSLC_PATH, SHADER_PATH], capture_output=True, text=True)
+    if result.stdout:
+        print(result.stdout[:-1])
+    if result.stderr:
+        print(result.stderr)
+
+    # re-enable reference nodes
+    for n in ref_nodes:
+        n.parm("enable1").set(True)
+
+
+# get all reference nodes
+ref_nodes = [n for n in hou.node("/stage").allSubChildren() if "reference" in n.type().name()]
+
+# disable reference nodes
+for n in ref_nodes:
+    n.parm("enable1").set(False)
+
+# compile and re-enable reference nodes on a separate thread
+t = Thread(target=compile_and_enable_references)
+t.start()
